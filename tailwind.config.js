93fe6f2649ac181ad386b4ff72ce71e29cd10806
Baldeabdoullah <abdoullahbalde@gmail.com>
2024-12-27 20:12:30 +0000

    tailwind custom

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    spacing: {
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#7deb65",
+          200: "#6ff252",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
